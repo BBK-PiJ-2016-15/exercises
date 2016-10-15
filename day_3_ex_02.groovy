4d2bf75c6affe053b49f7a5285fd6e43d1d92a5e
BBK-PiJ-2016-15 <federicoghersi@mbp-di-federico.home>
2016-10-15 21:34:42 +0100

    Day 03 Ex 02

---
+++ day_3_ex_02.groovy
@@ -0,0 +1,34 @@
+println "Type a number"
+String n = System.console()readLine()
+Double num1 = Integer.parseInt(n)
+println 'Please type a second number'
+String m = System.console()readLine()
+Double num2 = Integer.parseInt(m)
+
+
+println "Please, choose the operator"
+
+println "In order to choose an operator, type the corresponding letter: "
+println "A) +"
+println "B) -"
+println "C) *"
+println "D) /"
+
+String operator = System.console()readLine()
+
+if ((operator == "A") || (operator == "a")) {
+	println num1 + num2
+}
+else if ((operator == "B") || (operator == "b")){
+	println num1 - num2
+}
+else if ((operator == "C") || (operator == "c")){
+	println num1 * num2
+}
+else if ((operator == "D") || (operator == "d")){
+	println num1 / num2 
+}
+else {
+	println "Error! You had to choose one of the four options above."
+	println "This programme will stop running."
+}

49d2890712af3125ebc9b0f564d8efd984f0c0ab
BBK-PiJ-2016-15 <fghers01@dcs.bbk.ac.uk>
2016-10-31 19:34:39 +0000

    Day 5 exercise 1

---
+++ Day 5/Calculate.groovy
@@ -0,0 +1,12 @@
+
+Calculator calc = new Calculator()
+
+println calc.subtract(14,3)
+
+println calc.modulo(7,3)
+
+println calc.add(3,9.7)
+
+println calc.multiply(3,27.2)
+
+println calc.divide(18,5)
